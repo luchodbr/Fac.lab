98bd3c7cea404f3e562f3f9ab4d319c307f6edb6
Luciano Gil <lgil@crystalis-ar.local>
2019-11-07 17:11:05 -0300

    terminar

--- Excepciones/ClasesInstanciables/Jornada.cs
+++ Excepciones/ClasesInstanciables/Jornada.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Archivos;
 namespace ClasesInstanciables
 {
     public class Jornada
@@ -16,12 +16,12 @@ namespace ClasesInstanciables
             set { alumnos = value; }
         }
 
-        private Universidad.EClases clases;
+        private Universidad.EClases clase;
 
-        public Universidad.EClases Clases
+        public Universidad.EClases Clase
         {
-            get { return clases; }
-            set { clases = value; }
+            get { return clase; }
+            set { clase = value; }
         }
 
         private Profesor profesor;
@@ -33,14 +33,62 @@ namespace ClasesInstanciables
         }
 
         private Jornada()
-        { }
+        {
+            this.alumnos = new List<Alumno>();
+        }
 
         public Jornada(Universidad.EClases clase, Profesor instructor)
         {
 
         }
 
+        public static bool operator ==(Jornada j, Alumno a)
+        {
+            foreach (Alumno alumno in j.alumnos)
+            {
+                if (alumno == a)
+                    return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Jornada j, Alumno a)
+        {
+            return !(j == a);
+        }
 
+        public static Jornada operator +(Jornada j, Alumno a)
+        {
+            if (j != a)
+            {
+                j.alumnos.Add(a);
+                return j;
+            }
+            return j;
+        }
 
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Clase de " + this.clase);
+            sb.AppendFormat("NOMBRE COMPLETO {} ", this.Profesor.ToString());
+            foreach (Alumno alumno in this.Alumnos)
+            {
+                sb.AppendLine(alumno.ToString());
+            }
+            return sb.ToString();
+        }
+
+        public bool Guardar()
+        {
+            Texto t = new Texto();
+            return t.Guardar("Texto de jornada", this.ToString());
+            
+        }
+        public bool Leer()
+        {
+            Texto t = new Texto();
+            return t.Leer("Texto de jornada",out string s);
+
+        }
     }
 }
